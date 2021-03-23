7daf9a4cd851f345f613a51c6513ec33c0486b73
ivenpoker <nwaforhappiyvan@gmail.com>
2021-03-23 21:18:37 +0100

    Add circleci pipeline.
    Signed-off-by: ivenpoker <nwaforhappiyvan@gmail.com>

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -3,9 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
-  appName: "Udagram",
-  apiHost: "http://localhost:8080/api/v0",
+  production: false,
+  appName: 'Udagram',
+  apiHost: 'http://nd0067-udagram-api-app14-env.eba-gvdaejrx.us-east-1.elasticbeanstalk.com/api/v0'
 };
 
 /*
